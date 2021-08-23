1f5db96a78cd5bac27ec0e6613adfc33b5b50165
piazadinhadocurso <89218035+piazadinhadocurso@users.noreply.github.com>
2021-08-23 14:22:07 -0300

    Add files via upload

---
+++ Projeto Piazadinha do Curso/js/script.js
@@ -0,0 +1,7 @@
+$('.btnabre').click(function(){
+    $('.menulateral').togleClass('mostra');
+});
+$('.btnfecha').click(function(){
+    $('.menulateral').togleClass('mostra');
+});
+/*NAO TO USANDO, PODE APAGAR SE QUISER*/
